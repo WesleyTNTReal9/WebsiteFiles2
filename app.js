67d91be28441b1f8e6c5efd35e9106fb44b564ee
WesleyTNTReal9 <114668377+WesleyTNTReal9@users.noreply.github.com>
2023-02-12 19:03:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const pics = [
+    'url("Among_Us_Eye.png")',
+    'url("EGGHEAD.jpg")',
+    'url("Extreme_Strong_Bear.png")',
+    'url("Laughed_To_Hard.png")',
+    'url("Mammott_Icon.png")',
+    'url("Minion_Mammott_Icon.png")',
     'url("Monkey.gif")',
     'url("MRBEAST.jpg")',
-    
+    'url("Peter_Griffin.png")',
+    'url("Phlox.png")',
+    'url("Sans.png")',
+    'url("The_Rock.jpg")',
+
 
 ];
 
